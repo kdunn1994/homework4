ec81111c22f5e11661500514895bc40cf2ec5a4f
Kevin Dunn <kdunn1@stu.southtexascollege.edu>
2025-02-11 21:33:48 -0600

    completed transaction list component

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,11 @@
   import Balance from './components/Balance.vue';
   import IncomeExpenses from './components/IncomeExpenses.vue';
   import AddTransaction from './components/AddTransaction.vue';
+  import TransactionList from './components/TransactionList.vue';
+
   import {ref, computed} from 'vue';
   
-  const transactions = ref([
-    {id: 1, text:'Paycheck', amount: 700.00},
-    {id: 2, text:'Water Bill', amount: -72.83},
-    {id: 3, text:'Electric Bill', amount: -153.89},
-    {id: 4, text:'Returned Item', amount: 20.00},
-  ])
+  const transactions = ref([])
 
   const sum = computed(()=>{
     return transactions.value.reduce((acc, x)=>{
@@ -43,6 +40,15 @@
     })
   }
 
+  const generateID = () =>{
+    return Math.floor(Math.random()*100000000)
+  }
+
+  const handleDelete = (id) => {
+    transactions.value = transactions.value.filter((x) => x.id !== id)
+  }
+
+
 </script>
 
 <template>
@@ -51,6 +57,7 @@
     <Balance :total="sum"></Balance>
     <IncomeExpenses :income="moneyIn" :expense="moneyOut"></IncomeExpenses>
     <AddTransaction @transactionSubmitted="handleTransaction"></AddTransaction>
+    <TransactionList :transactions="transactions" @transactionDeleted="handleDelete"></TransactionList>
   </div>
 
 </template>
